eceaba050376ff56fec0be1f53e3ec6f9157b167
imelati15 <larasmlt05@gmail.com>
2025-11-01 18:02:45 +0700

    update file

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ with app.app_context():
 def home():
     return jsonify({"status":"ok","message":"Backend ready 🚀"})
 
+port = int(os.environ.get('PORT', 5000))
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host="0.0.0.0", port=port)
