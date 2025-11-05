82f4bbe4bf4dc7752d3e7db10e9e90f769fa0a7f
imelati15 <larasmlt05@gmail.com>
2025-11-05 16:24:10 +0700

    update app

--- app.py
+++ app.py
@@ -1,32 +1,31 @@
 from flask import Flask, jsonify
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
 
-# Gunakan environment variables
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
 
-# Init extensions
 from config.db import db, ma
 from config.extensions import bcrypt, jwt
+
+CORS(app)  
 
 db.init_app(app)
 ma.init_app(app)
 bcrypt.init_app(app)
 jwt.init_app(app)
 
-# Register blueprint
 from web import api
 app.register_blueprint(api)
 
-# Pindah import models ke dalam app context
+
 with app.app_context():
-    # Import models setelah app dan db di-initialize
     from models import user_model, order_model, menu_model, notification_model
     db.create_all()
 
--- web.py
+++ web.py
@@ -8,22 +8,22 @@ from controllers.notification_controller import (
     mark_all_notifications_as_read
 )
 
-api = Blueprint('api', __name__)
+api = Blueprint('api', __name__, url_prefix='/api')
 
 # User & Auth
-api.route('/api/auth/register', methods=['POST'])(register_user)
-api.route('/api/auth/login', methods=['POST'])(login_user)
-api.route('/api/profile', methods=['GET'])(get_user_profile)
+api.route('/auth/register', methods=['POST'])(register_user)
+api.route('/auth/login', methods=['POST'])(login_user)
+api.route('/profile', methods=['GET'])(get_user_profile)
 
 # Menu
-api.route('/api/menus', methods=['GET'])(get_all_menus)
-api.route('/api/menu/<int:menu_id>', methods=['GET'])(get_menu_by_id)
+api.route('/menus', methods=['GET'])(get_all_menus)
+api.route('/menu/<int:menu_id>', methods=['GET'])(get_menu_by_id)
 
 # Order
-api.route('/api/orders', methods=['POST'])(create_order)
-api.route('/api/orders', methods=['GET'])(get_user_orders)
+api.route('/orders', methods=['POST'])(create_order)
+api.route('/orders', methods=['GET'])(get_user_orders)
 
 # Notification
-api.route('/api/notifications', methods=['GET'])(get_user_notifications)
-api.route('/api/notifications/<int:notif_id>/read', methods=['PUT'])(mark_notification_as_read)
-api.route('/api/notifications/read-all', methods=['PUT'])(mark_all_notifications_as_read)
+api.route('/notifications', methods=['GET'])(get_user_notifications)
+api.route('/notifications/<int:notif_id>/read', methods=['PUT'])(mark_notification_as_read)
+api.route('/notifications/read-all', methods=['PUT'])(mark_all_notifications_as_read)
